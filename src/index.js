c1d7220f436d179f75486aa78cf694daf2595e9e
DBenson1017 <devinbenson@gmail.com>
2020-09-09 17:54:05 -0400

    Wed EOD

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(e){
     loadLandingPageData()
+    stateLoader()
     // stateData()
     // userList()
 })
 
 //#################### constants ##################################
 const stateList = []
+
+
+// listener on state drop down
+document.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log(e.target)
+    if(e.target.id === 'state-selector'){
+        let form = document.querySelector('#state-selector')
+        populateReportData(form[0].value)
+    } else if (e.target.id==='report-selector'){
+        console.log('build report clicked')
+        renderReportSelection()
+        saveReport()
+    }
+})
 
 // initial US landing page data
 function loadLandingPageData(){
@@ -43,28 +59,6 @@ function renderLPD(data){
     currentlyH.innerText = currentlyHospitalized
 }
 
-
-// const stateData = () => {
-//     fetch('https://api.covidtracking.com/v1/states/current.json')
-//         .then(function(response){
-//             return response.json()
-//         })
-//         .then(function (data){
-//             buildStateList(data)
-//         })
-// } // end of stateData
-
-// function buildStateList(data){
-//     console.log(data[0].state)
-//     for(state of data){
-//         stateList.push(state.state)
-//         // console.log(stateList)
-//     }
-//     makeStateSelection()
-// }//end of buildStateList
-
-
-
 function makeStateDropDown(state){
     console.log(state)
     let list= document.querySelector('#state-drop-down')
@@ -72,106 +66,123 @@ function makeStateDropDown(state){
     newS.setAttribute('value', state)
     newS.innerText=state.value
     list.append(newS)
-    console.log(newS)
 }
 
 
-
-
-// function makeStateSelection(stateList){
-//     stateList.forEach(makeStateDropDown)
-// }
-
-// const userList =() => {
-//     fetch('http://localhost:3000/users')
-//         .then(function(response){
-//             return response.json()
-//         })
-//         .then(function(data){
-//             console.log(data)
-//         })
-//     }
-
-// ###################### 
-
-//state API 
-// listener on state drop down
-document.addEventListener('submit', function(e){
-    e.preventDefault()
-    console.log(e.target)
-    if(e.target.id === 'state-selector'){
-        let form = document.querySelector('#state-selector')
-        console.log(form[0].value)
-        populateReportData(form[0].value)
-    }
-})
-//state fetch based on drop down 
+//############# state-based data selector ###################
 function populateReportData(state){
     fetch(`https://api.covidtracking.com/v1/states/${state}/current.json`)
         .then(function(response){
             return response.json()
         })
         .then(function(data){
-            // console.log(data)
-            destructureData(data)
+            console.log(data)
+            displayData(data)
         })
 }
 
 
-function destructureData(hash){
-    
-    let newData = {
-        dataQualityGrade,
-        date,
-        death,
-        deathIncrease,
-        deathProbable,
-        hospitalizedCumulative,
-        hospitalizedCurrently,
-        inIcuCurrently,
-        onVentilatorCurrently,
-        negative, 
-        positive, 
-        positiveCasesViral,
-        recovered, 
-        totalTestResults, 
-        totalTestsAntiboby, 
-        positiveTestsAntibody
-    } = hash 
-    console.log(hash)
-    console.log(newData)
-    
+function displayData(hash){
+    let fields = [
+        'dataQualityGrade',
+        'date',
+        'death',
+        'deathIncrease',
+        'deathProbable',
+        'hospitalizedCumulative',
+        'hospitalizedCurrently',
+        'inIcuCurrently',
+        'onVentilatorCurrently',
+        'negative', 
+        'positive', 
+        'positiveCasesViral',
+        'recovered', 
+        'totalTestResults', 
+        'totalTestsAntiboby', 
+        'positiveTestsAntibody'
+    ]
+     for (const key in hash){
+    // console.log(`${key}:${hash[key]}`)
+        if(fields.includes(`${key}`)){
+            let reportSelector = document.querySelector('#report-selector')
+            
+            let box = document.createElement('input')
+            box.type='checkbox'
+            box.name =`${key}`
+            box.value = hash[key]
+            
+            let lbl = document.createElement('label')
+            lbl.for = box.name
+            lbl.innerText =`${key}`
 
+            let br = document.createElement('br')
 
+            lbl.append(box)
+            lbl.append(br)
+            reportSelector.append(lbl)
+        }
+    }
 }
 
-function renderReportSelector(){
-    let form = document.querySelector('#report-selector')
+function renderReportSelection(){
+    //###### builds table structure
+    let reportTable=document.querySelector('#report-table')
 
+    // ##### populates table with selected values
+    let selections = document.querySelectorAll('input:checked')
+    console.dir(selections)
+    for(element of selections){
 
+    console.log(element.attributes[1].nodeValue)
+    let newRow = document.createElement('tr')
 
+    let newKey = document.createElement('td')
+    newKey.innerText = element.attributes[1].nodeValue
+    
+    let newValue = document.createElement('td')
+    newValue.innerText = element.attributes[2].nodeValue
+    debugger
+    
+    newRow.append(newKey)
+    newRow.append(newValue)
+    reportTable.append(newRow)
+    } // end of forEach 
+} // end of render Report Selection 
 
+//save report to rails API 
+function saveReport(){
+    //take selections to make data object 
+    // make configOb with data object
+    // pass into a post request fetch 
 
-}
+    let selections = document.querySelectorAll('input:checked')
+    let data = {
+        
+    }
+    for(element of selections){
+        let key= `${element.attributes[1].nodeValue}`
+        let value = parseInt(element.attributes[2].nodeValue, 10)
+        
+        console.log(key)
+        console.log(value)
+        data[key]=value
 
-
-
-
-
-
-
-
-
-
-fetch('https://api.covidtracking.com/v1/states/oh/current.json')
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
         console.log(data)
-    })
+
+    }
+} // end of saveReport 
+
+
+
+
+
+
+
+
+
 
 //########### build state list #################
+function stateLoader(){
     fetch('https://api.covidtracking.com/v1/states/current.json')
     .then(function(response){
         return response.json()
@@ -179,6 +190,7 @@ fetch('https://api.covidtracking.com/v1/states/oh/current.json')
     .then(function(data){
         separateStates(data)
     })
+}
 
     function separateStates(array){
         array.forEach(buildStateList)
@@ -192,3 +204,23 @@ fetch('https://api.covidtracking.com/v1/states/oh/current.json')
         document.querySelector('#state-dropdown').append(selection)
     }
     //#########################
+
+
+    // let fields = [
+    //     'dataQualityGrade',
+    //     date,
+    //     death,
+    //     deathIncrease,
+    //     deathProbable,
+    //     hospitalizedCumulative,
+    //     hospitalizedCurrently,
+    //     inIcuCurrently,
+    //     onVentilatorCurrently,
+    //     negative, 
+    //     positive, 
+    //     positiveCasesViral,
+    //     recovered, 
+    //     totalTestResults, 
+    //     totalTestsAntiboby, 
+    //     positiveTestsAntibody
+    // ]
